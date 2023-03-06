2d8316605552f5d877f16839ad4222ee0b67fdf3
Josue Fleitas <josue.fle.sanc@gmail.com>
2023-03-06 14:32:22 +0000

    Small Syntax Refactor

--- app/src/app.py
+++ app/src/app.py
@@ -14,15 +14,10 @@ from src.endpoints.blueprint_register import blueprint_register  #TODO: probably
 
 # Initialize Flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 jwt = JWTManager(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-
-# Set up config
-app.config["SECRET_KEY"] = Config.SECRET_KEY
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = Config.JWT_ACCESS_TOKEN_EXPIRES
-app.config["JWT_SECRET_KEY"] = Config.JWT_SECRET_KEY
-app.config['CORS_HEADERS'] = Config.JWT_SECRET_KEY
 
 # Register blueprints, ensuring paths are versioned
 app.register_blueprint(blueprint_deliveries, url_prefix="/api/v1/deliveries")
